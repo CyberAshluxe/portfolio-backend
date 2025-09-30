2fbf512b0ed6032808fd33c01b4cfa22cd233bee
Ashrof <ashrofabdulrofiu21.com>
2025-09-30 14:44:20 +0100

    port added

--- index.js
+++ index.js
@@ -34,3 +34,5 @@
             res.status(500).send('Error sending email.');
         }
     });
+    const PORT = process.env.PORT || 5880;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
